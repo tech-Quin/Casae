e8ec2383c0a5dc076f2f1f9291bc598e2edbc0fb
tech-Quin <joaquinestapia0@gmail.com>
2025-04-25 22:56:28 +0800

    Add files via upload

---
+++ final.js
@@ -0,0 +1,11 @@
+function toggleInterior() {
+    const gallery = document.getElementById('interiorGallery');
+    if (gallery.style.display === 'none' || gallery.style.display === '') {
+      gallery.style.display = 'block';
+    } else {
+      gallery.style.display = 'none';
+    }
+  }
+  
+
+      
